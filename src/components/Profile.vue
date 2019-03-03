a4859f7933a5392b770d9feca4d78c144f96c4c0
Dabit <dabit@8c85901b6e1c.ant.amazon.com>
2019-03-03 09:07:00 -0800

    removed "this" reference

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1>Welcome, {{this.user.username}}</h1>
+  <h1>Welcome, {{user.username}}</h1>
 </template>
 
 <script>
@@ -21,6 +21,3 @@ export default {
   }
 }
 </script>
-
-<style>
-</style>
